912baf7783cfc03696633a99946c9d253141381e
Alon Swartz <alon@sterilesecurity.com>
2008-07-03 15:47:30 +0300

    added logging to common.system
    rational:
        - system exits on failure (we need this for debconf), so we should
          atleast log the failure
        - also provide logging interface for components

--- di-live.d/common.py
+++ di-live.d/common.py
@@ -2,12 +2,21 @@
 import os
 import sys
 
+LOGFILE = '/var/log/di-live.log'
+def log(s):
+    file(LOGFILE, 'a').write(s + "\n")
+
 def prepend_path(path):
     os.environ['PATH'] = path + ":" + os.environ.get('PATH')
 
 def system(command):
-    """if command returns non-zero exitcode, exit with it"""
+    """if command returns non-zero exitcode, exit with it
+    not a good coding practice, but we need this for debconf
+    """
     error = os.system(command)
     if error:
-        sys.exit(os.WEXITSTATUS(error))
+        exitcode = os.WEXITSTATUS(error)
+        log("non-zero exitcode (%d) for command: %s" % (exitcode, command))
+        sys.exit(exitcode)
+
 
