539eae96e4b18574ffa963da1590bce1953f45e5
Alon Swartz <alon@sterilesecurity.com>
2008-07-04 13:31:37 +0300

    cleaned up command execution
    - using code from executil
    - created convenience function for executing dilive debconf related
      commands

--- di-live.d/common.py
+++ di-live.d/common.py
@@ -1,6 +1,9 @@
 
 import os
 import sys
+import commands
+
+mkarg = commands.mkarg
 
 LOGFILE = '/var/log/di-live.log'
 def log(s):
@@ -9,14 +12,46 @@ def log(s):
 def prepend_path(path):
     os.environ['PATH'] = path + ":" + os.environ.get('PATH')
 
-def system(command):
-    """if command returns non-zero exitcode, exit with it
-    not a good coding practice, but we need this for debconf
+class ExecError(Exception):
+    """Accessible attributes:
+    command     executed command
+    exitcode    non-zero exitcode returned by command
     """
+    def __init__(self, command, exitcode):
+        Exception.__init__(self, command, exitcode)
+        self.command = command
+        self.exitcode = exitcode
+
+    def __str__(self):
+        str = "non-zero exitcode (%d) for command: %s" % (self.exitcode,
+                                                          self.command)
+        return str
+
+def fmt_command(command, *args):
+    return command + " ".join([mkarg(arg) for arg in args])
+
+def system(command, *args):
+    """Executes <command> with <*args> -> None
+    If command returns non-zero exitcode raises ExecError"""
+
+    sys.stdout.flush()
+    sys.stderr.flush()
+
+    command = fmt_command(command, *args)
     error = os.system(command)
     if error:
         exitcode = os.WEXITSTATUS(error)
-        log("non-zero exitcode (%d) for command: %s" % (exitcode, command))
-        sys.exit(exitcode)
+        raise ExecError(command, exitcode)
 
+def dilive_system(command, *args):
+    """convenience function for di-live debconf related command execution
+    - prepend compat to path
+    - catch and log execution exception, exit with exitcode
+    """
+    prepend_path('/usr/lib/di-live/compat')
+    try:
+        system(command, *args)
+    except ExecError, e:
+        log(str(e))
+        sys.exit(e.exitcode)
 
