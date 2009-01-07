20e7a0b3e0bbcb55a63b4fd0c0eae3418a2068a8
Alon Swartz <alon@sterilesecurity.com>
2009-01-07 15:57:22 +0200

    moved DeconfPass related code from mysqlpass into common
    rational:
        - pgsqlpass also needs to use it

--- di-live.d/common.py
+++ di-live.d/common.py
@@ -11,8 +11,64 @@ LOGFILE = '/var/log/di-live.log'
 def log(s):
     file(LOGFILE, 'a').write(s + "\n")
 
-def prepend_path(path):
-    os.environ['PATH'] = path + ":" + os.environ.get('PATH')
+class Chroot:
+    def __init__(self, newroot, environ={}):
+        self.environ = { 'HOME': '/root',
+                         'TERM': os.environ['TERM'],
+                         'LC_ALL': 'C',
+                         'PATH': "/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/bin:/usr/sbin" }
+        self.environ.update(environ)
+        self.path = os.path.realpath(newroot)
+
+    def _prepare_command(self, *command):
+        env = ['env', '-i' ] + [ name + "=" + val
+                                 for name, val in self.environ.items() ]
+
+        command = fmt_command(*command)
+        return ("chroot", self.path, 'sh', '-c', " ".join(env) + " " + command)
+
+    def system(self, *command):
+        """execute system command in chroot -> None"""
+        system(*self._prepare_command(*command))
+
+class DebconfPass:
+    def __init__(self, package):
+        self.package = package
+
+        debconf.runFrontEnd()
+        self.db = debconf.Debconf()
+        self.db.capb('backup')
+
+        self.password = ""
+
+    def __del__(self):
+        self.db.stop()
+
+    def _db_input(self, template):
+        template = "%s/%s" % (self.package, template)
+        self.db.reset(template)
+        self.db.input(debconf.HIGH, template)
+        try:
+            self.db.go()
+        except debconf.DebconfError, e:
+            self.db.stop()
+            sys.exit(e[0])
+
+        ret = self.db.get(template)
+        self.db.reset(template)
+        return ret
+
+    def ask(self, password, password_again, allow_empty=True):
+        while 1:
+            self.password = self._db_input(password)
+            if self.password == self._db_input(password_again):
+                if not self.password and not allow_empty:
+                    self._db_input('password_empty')
+                    continue
+                
+                break
+
+            self._db_input('password_mismatch')
 
 class ExecError(Exception):
     """Accessible attributes:
@@ -28,6 +84,9 @@ class ExecError(Exception):
         str = "non-zero exitcode (%d) for command: %s" % (self.exitcode,
                                                           self.command)
         return str
+
+def prepend_path(path):
+    os.environ['PATH'] = path + ":" + os.environ.get('PATH')
 
 def fmt_command(command, *args):
     return command + " ".join([mkarg(arg) for arg in args])
