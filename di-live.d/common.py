32e94fc3f15fe52414e5a323eb003bc943b006dd
Alon Swartz <alon@sterilesecurity.com>
2009-09-03 22:23:12 +0300

    support passwords with special characters (eg. pas$word - shell interpolation)

--- di-live.d/common.py
+++ di-live.d/common.py
@@ -1,5 +1,6 @@
 # Copyright (c) 2008 Alon Swartz <alon@turnkeylinux.org> - all rights reserved
 
+import re
 import os
 import sys
 import debconf
@@ -70,7 +71,7 @@ class Debconf:
 
             self._db_input('di-live/password_mismatch')
 
-        return password
+        return re.escape(password)
 
 
 class ExecError(Exception):
