3ad7ed2d18be94c6efa5970efb108e95eca408dd
Jeremy Davis <jeremy@turnkeylinux.org>
2025-10-16 10:14:44 +1100

    clean templates more carefully and retain non-translation related comments

--- build-di-src.py
+++ build-di-src.py
@@ -4,7 +4,7 @@ import argparse
 import os
 import shutil
 import subprocess
-from os.path import exists, join
+from os.path import exists, isdir, join
 
 packages = (
     "base-installer",
@@ -31,6 +31,17 @@ packages = (
 
 class DiSrcError(Exception):
     pass
+
+
+def clean_tmp_dir(force: bool = False) -> None:
+    if force or not isdir("tmp"):
+        shutil.rmtree("tmp", ignore_errors=True)
+        os.makedirs("tmp")
+        return
+    tmp_dirs = [item.path for item in os.scandir("tmp") if item.is_dir()]
+    if tmp_dirs:
+        for tmp_dir in tmp_dirs:
+            shutil.rmtree(tmp_dir)
 
 
 def get_version(package: str, suite: str = "stable") -> str:
@@ -67,7 +78,46 @@ def get_version(package: str, suite: str = "stable") -> str:
     return found_version
 
 
-def get_src(package: str, version: str) -> None:
+def clean_template(template_path: str, verbose: bool = False) -> None:
+    """Remove translation related placeholders and comments."""
+    def echo(msg: str) -> None:
+        if verbose:
+            print(msg)
+    lines = []
+    with open(template_path) as fob:
+        for line in fob:
+            if line.startswith("_"):
+                if line.startswith("_Description:"):
+                    line = line[1:]
+                elif line.startswith("__Choices:"):
+                    line = line[2:]
+                if "[" in line:
+                    echo("   * cleaning line:")
+                    echo(f"    - {line}")
+                    new_line = ""
+                    for char in line:
+                        if char == "[":
+                            new_line = new_line + "\n"
+                            break
+                        new_line = new_line + char
+                    line = new_line
+                    echo("   * new line:")
+                    echo(f"    - {line}")
+            elif line.startswith("#"):
+                if (
+                    line.startswith("# :sl")
+                    or line.startswith("#flag:translate")
+                    or "translate" in line.lower()
+                ):
+                    echo("   * ignoring line:")
+                    echo(f"    - {line}")
+                    continue
+            lines.append(line)
+    with open(template_path, "w") as fob:
+        fob.writelines(lines)
+
+
+def get_src(package: str, version: str, verbose_clean: bool = False) -> None:
     os.makedirs("tmp", exist_ok=True)
     dsc = f"{package}_{version}.dsc"
     url = join(
@@ -92,23 +142,18 @@ def get_src(package: str, version: str) -> None:
     local_src = join("d-i/source", package)
     if exists(local_src):
         shutil.rmtree(local_src)
+    print(f"* Processing {package} source code")
     shutil.move(join("tmp", f"{package}-{version}"), local_src)
     for base, dirnames, filenames in os.walk(local_src):
+        # remove translations - we only support english
         if "po" in dirnames:
+            print(f"* Removing {package} translations")
             shutil.rmtree(join(base, "po"))
         for file in filenames:
             if file.endswith("templates"):
-                file = join(base, file)
-                lines = []
-                with open(file) as fob:
-                    for line in fob:
-                        if line.startswith("_"):
-                            line = line[1:]
-                        elif line.startswith("#"):
-                            continue
-                        lines.append(line)
-                with open(file, "w") as fob:
-                    fob.writelines(lines)
+                template = join(base, file)
+                print(f"* Cleaning {package} d-i template: {template}")
+                clean_template(template, verbose=verbose_clean)
 
 
 def main() -> None:
@@ -120,12 +165,29 @@ def main() -> None:
         help="suite or codename to download source package for (default:"
         " 'stable')",
     )
+    parser.add_argument(
+        "-f",
+        "--force-download",
+        action="store_true",
+        help="force download of source packages (will reuse cached copies by"
+        " if they exist by default)"
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose-template-clean",
+        action="store_true",
+        help="enable verbose messages when cleaning templates - useful for"
+        " debugging template issues"
+    )
     args = parser.parse_args()
+    clean_tmp_dir(force=args.force_download)
     for package in packages:
-        print(f"Checking for source package: {package}")
+        print(f"* Checking for source package: {package}")
         version = get_version(package, args.suite)
-        print(f"Downloading source package version: {version}")
-        get_src(package, version)
+        print(
+            f"* Downloading and/or verfiying source package version: {version}"
+        )
+        get_src(package, version, verbose_clean=args.verbose_template_clean)
 
 
 if __name__ == "__main__":
