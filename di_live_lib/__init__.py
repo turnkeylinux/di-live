431b1d4e9cf06922497ef4257987866c0d9cac2a
Jeremy Davis <jeremy@turnkeylinux.org>
2025-10-17 14:19:13 +1100

    Add _heaps_ of logging...

--- di_live_lib/__init__.py
+++ di_live_lib/__init__.py
@@ -15,9 +15,13 @@ LOG_FILE = "/var/log/di-live.log"
 
 def setup_logging(
     log_file: str = LOG_FILE,
-    log_level: int = logging.INFO,
+    log_level: int | None = None,
 ) -> logging.Logger:
     """Configure di-live logging - call once per 'di-live.d/' script."""
+    if log_level is None:
+        log_level = int(
+            os.getenv("DEBUG", os.getenv("DILIVE_DEBUG", logging.INFO))
+        )
     logger = logging.getLogger()
     # ensure no duplicate handlers - just in case...
     if logger.handlers:
@@ -45,6 +49,7 @@ class Chroot:
     def __init__(
         self, newroot: str, environ: dict[str, str] | None = None
     ) -> None:
+        logger.debug(f"Chroot.__init__({newroot=}, {environ=})")
         if environ is None:
             environ = {}
         # hack to allow exiting the chroot later - see rest in __del__()
@@ -74,16 +79,19 @@ class Chroot:
         stdout: int | None = None,
     ) -> None:
         """Leverages system function to execute command in chroot."""
+        logger.debug(f"Chroot.system({command=}, {shell=}, {stdout=})")
         system(command, shell=shell, stdout=stdout, env=self.environ)
 
     def exit(self) -> None:
         # hack to escape python chroot and get back to initial cwd
+        logger.debug("Chroot.exit()")
         os.fchdir(self.real_root)
         os.chroot(".")
         os.chdir(self.initial_cwd)
         self.targetmounts.umount()
 
     def __del__(self) -> None:
+        logger.debug("Chroot.__del__()")
         self.exit()
 
 
@@ -95,10 +103,12 @@ class DiliveDebconf:
     """
 
     def __init__(self, title: str | None = None) -> None:
+        logger.debug(f"DiliveDebconf.__init__({title=})")
         if title:
             self.db = debconf.Debconf(run_frontend=True, title=title)
         else:
             self.db = debconf.Debconf(run_frontend=True)
+        logger.debug('DiliveDebconf.db.capb("backup")')
         self.db.capb("backup")
 
     def _db_input(
@@ -107,19 +117,32 @@ class DiliveDebconf:
         description: str | None = None,
         default: str | None = None,
     ) -> str:
+        logger.debug(
+            f"DiliveDebconf._db_input({template=}, {description=}, {default=})"
+        )
+        logger.debug(f"DiliveDebconf.db.reset({template})")
         self.db.reset(template)
         if description:
+            logger.debug(
+                f"DiliveDebconf.db.subst('{template}', 'DESCRIPTION',"
+                f" '{description}')"
+            )
             self.db.subst(template, "DESCRIPTION", description)
         if default:
+            logger.debug(f"DiliveDebconf.db.set('{template}', '{default}')")
             self.db.set(template, default)
+        logger.debug(f"DiliveDebconf.db.input({debconf.HIGH}, {template})")
         self.db.input(debconf.HIGH, template)
         try:
+            logger.debug("DiliveDebconf.db.go()")
             self.db.go()
         except debconf.DebconfError as e:
+            logger.debug("DiliveDebconf.db.stop()")
             self.db.stop()
             sys.exit(e.args[0])
 
         ret = self.db.get(template)
+        logger.debug(f"DiliveDebconf.db.reset({template})")
         self.db.reset(template)
         return ret
 
@@ -129,9 +152,15 @@ class DiliveDebconf:
         description: str | None = None,
         default: str | None = None,
     ) -> str:
+        logger.debug(
+            f"DiliveDebconf.get_input({template=}, {description=}, {default=})"
+        )
         return self._db_input(template, description, default)
 
     def get_password(self, description: str, allow_empty: bool = False) -> str:
+        logger.debug(
+            f"DiliveDebconf.get_password({description=}, {allow_empty=})"
+        )
         while 1:
             password = self._db_input("di-live/password", description)
             if password == self._db_input("di-live/password_again"):
@@ -151,21 +180,35 @@ class DiliveDebconf:
         template: str = "di-live/progress/generic",
         description: str | None = None,
     ) -> None:
+        logger.debug(
+            f"DiliveDebconf.progress_init({steps=}, {template=},"
+            f" {description=})"
+        )
         if description:
+            logger.debug(
+                f"DiliveDebconf.db.subst('{template}', 'DESCRIPTION',"
+                f" '{description}')"
+            )
             self.db.subst(template, "DESCRIPTION", description)
         self.progress_steps = steps
-        self.db.progress(f"START 0 {steps} {template}")
+        progress = f"START 0 {steps} {template}"
+        logger.debug(f"DiliveDebconf.db.progress({progress})")
+        self.db.progress(progress)
 
     def progress_step(self, step_no: int) -> None:
+        logger.debug(f"DiliveDebconf.progress_step({step_no=})")
         if step_no > self.progress_steps:
             return
-        self.db.progress(f"STEP {step_no}")
+        progress = f"STEP {step_no}"
+        logger.debug(f"DiliveDebconf.db.progress({progress})")
+        self.db.progress(progress)
 
 
 class TargetMounts:
     """Set up and remove required mountpoints for a chroot."""
 
     def __init__(self, target: str = "/target") -> None:
+        logger.debug(f"TargetMounts.__init__({target=})")
         self.targetdev = os.path.join(target, "dev")
         self.targetproc = os.path.join(target, "proc")
         self.targetsys = os.path.join(target, "sys")
@@ -173,6 +216,7 @@ class TargetMounts:
         self.mount()
 
     def mount(self) -> None:
+        logger.debug("TargetMounts.mount()")
         def _mount(primary_dir: str, secondary_dir: str) -> None:
             system(["mount", "-o", "bind", primary_dir, secondary_dir])
 
@@ -184,6 +228,7 @@ class TargetMounts:
             _mount("/sys", self.targetsys)
 
     def umount(self) -> None:
+        logger.debug("TargetMounts.umount()")
         def _umount(mounted_dir: str) -> None:
             system(["umount", "-f", mounted_dir])
 
@@ -205,6 +250,7 @@ class TargetMounts:
             _umount(self.targetrun)
 
     def __del__(self) -> None:
+        logger.debug("TargetMounts.__del__()")
         self.umount()
 
 
@@ -216,6 +262,7 @@ class ExecError(Exception):
     """
 
     def __init__(self, command: str, exitcode: int) -> None:
+        logger.debug(f"ExecError.__init__({command=}, {exitcode=})")
         Exception.__init__(self, command, exitcode)
         self.command = command
         self.exitcode = exitcode
@@ -227,6 +274,7 @@ class ExecError(Exception):
 
 
 def prepend_path(path: str) -> None:
+    logger.debug(f"prepend_path({path=})")
     os.environ["PATH"] = f"{path}:{os.getenv('PATH', '')}"
 
 
@@ -244,6 +292,7 @@ def system(
      - a list containing individual items/arguments; or
      - if shell=True, a list containing a single string.
     """
+    logger.debug(f"system({command=}, {shell=}, {stdout=}, {env=})")
     if env is None:
         env = dict(os.environ)
     if shell and isinstance(command, list):
@@ -272,6 +321,7 @@ def dilive_system(command: list[str]) -> None:
     - catch and log execution exception, exit with exitcode
     - stderr is redirected to the logfile by system()
     """
+    logger.debug(f"dilive_system({command=})")
     prepend_path("/usr/lib/di-live/compat")
     try:
         system(command)
@@ -285,22 +335,29 @@ def preset_debconf(
     preseeds: Sequence[tuple[str, str | bool]] | None = None,
     seen: list[tuple[str, bool]] | None = None,
 ) -> None:
+    logger.debug(f"preset_debconf({resets=}, {preseeds=}, {seen=})")
     db = debconf.Debconf(run_frontend=True)
 
     if resets:
         for template in resets:
+            logger.debug(f"debconf.Debconf().reset({template=})")
             db.reset(template)
 
     if preseeds:
         for template, value in preseeds:
+            logger.debug(f"debconf.Debconf().set({template=}, {value=})")
             db.set(template, value)
 
     if seen:
         for template, value in seen:
+            logger.debug(
+                f"debconf.Debconf().fset({template=}, 'seen', {value=})"
+            )
             db.fset(template, "seen", value)
 
 
 def is_mounted(directory: str) -> bool:
+    logger.debug(f"is_mounted({directory})")
     with open("/proc/mounts") as fob:
         for line in fob.readlines():
             _host, guest, *_others = line.split(" ")
@@ -310,10 +367,18 @@ def is_mounted(directory: str) -> bool:
 
 
 def target_mounted(target: str = "/target") -> bool:
+    logger.debug(f"target_mounted({target=})")
     if not os.path.exists(target) or not is_mounted(target):
         db = debconf.Debconf(run_frontend=True)
+        dconf = "debconf.Debconf()"
+        logger.debug(f"{dconf}.capb('backup')")
         db.capb("backup")
+        logger.debug(
+            f"{dconf}.input({debconf.CRITICAL},"
+            " 'base-installer/no_target_mounted')"
+        )
         db.input(debconf.CRITICAL, "base-installer/no_target_mounted")
+        logger.debug(f"{dconf}.go()")
         db.go()
         return False
     return True
